1fb350b793c8c312a105db57fa10bf148d3c623f
ivayloK <qwerty36758@gmail.com>
2023-07-19 17:58:12 +0300

    Added dynamic view with server data

--- src/app/reviews-list/reviews-list.component.ts
+++ src/app/reviews-list/reviews-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { IReview } from '../interfaces/review';
 
 @Component({
   selector: 'app-reviews-list',
   templateUrl: './reviews-list.component.html',
-  styleUrls: ['./reviews-list.component.css']
+  styleUrls: ['./reviews-list.component.css'],
 })
 export class ReviewsListComponent {
+  reviewsList: IReview[] | null = null;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getReviews().subscribe({
+      next: (value) => {
+        this.reviewsList = Object.values(value);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
