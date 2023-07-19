60913b709b1d3890ecafaff690837791e1093d9e
ivayloK <qwerty36758@gmail.com>
2023-07-19 18:25:37 +0300

    Updated reviews list component html and added dynamic recent reviews list component view with server data

--- src/app/recent-reviews-list/recent-reviews-list.component.ts
+++ src/app/recent-reviews-list/recent-reviews-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { IReview } from '../interfaces/review';
 
 @Component({
   selector: 'app-recent-reviews-list',
   templateUrl: './recent-reviews-list.component.html',
-  styleUrls: ['./recent-reviews-list.component.css']
+  styleUrls: ['./recent-reviews-list.component.css'],
 })
 export class RecentReviewsListComponent {
+  reviewsList: IReview[] | null = null;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getReviews().subscribe({
+      next: (value) => {
+        this.reviewsList = Object.values(value).slice(0, 3);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
